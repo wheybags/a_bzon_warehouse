47458a0ed535986032c5590db09fe163ba643013
Tom Mason <wheybags@wheybags.com>
2020-04-18 15:32:26 +0200

    bolierplate + add assets from https://www.kenney.nl/assets/generic-items

---
+++ items.lua
@@ -0,0 +1,82 @@
+local items = {}
+
+items.items =
+{
+  tools =
+  {
+    big =
+    {
+      pickaxe = 21,
+      axe = 20,
+      hammer = 23,
+      saw = 16
+    },
+
+    small =
+    {
+      wrench = 7,
+      screwdriver = 5,
+      pliers = 9,
+      brush = 11,
+    },
+  },
+
+  electronics =
+  {
+    computer =
+    {
+      black = 54,
+      white = 55,
+      laptop = 50,
+      screen = 53,
+    },
+
+    phone =
+    {
+      retro = 63,
+      flip = 64,
+      smart = 67,
+      tablet = 69,
+    },
+  },
+
+  kitchen =
+  {
+    utensils =
+    {
+      fork = 129,
+      spatula = 135,
+      knife = 134,
+      rolling_pin = 140,
+    },
+
+    appliances =
+    {
+      kettle = 125,
+      blender = 137,
+      toaster = 138,
+      coffee_machine = 139
+    },
+  },
+
+  medical =
+  {
+    drugs =
+    {
+      red_pill = 89,
+      blue_pill = 90,
+      white_pill = 96,
+      pink_pill = 97,
+    },
+
+    equipment =
+    {
+      stethoscope = 110,
+      crutch = 113,
+      first_aid_kit = 102,
+      syringe = 93,
+    },
+  }
+}
+
+return items
---
+++ main.lua
@@ -0,0 +1,75 @@
+local state
+
+local music
+local music_playing = false
+play_music = function()
+  love.audio.play(music)
+  music_playing = true
+end
+stop_music = function()
+  love.audio.stop(music)
+  music_playing = false
+end
+
+
+local setup = function()
+  --music = love.audio.newSource("/sfx/2019-12-09_-_Retro_Forest_-_David_Fesliyan.mp3", "stream")
+  --music:setLooping(true)
+  --
+  --play_music()
+end
+
+function love.load()
+  setup()
+end
+
+function love.draw()
+
+end
+
+function love.resize()
+
+end
+
+function love.keypressed(key)
+
+end
+
+
+function love.mousemoved(x,y)
+
+end
+
+function love.wheelmoved(x,y)
+
+end
+
+function love.mousepressed(x,y,button)
+
+end
+
+function love.quit()
+
+end
+
+local fixed_update = function()
+  if state == nil then
+    return
+  end
+
+
+end
+
+
+local accumulatedDeltaTime = 0
+function love.update(deltaTime)
+  accumulatedDeltaTime = accumulatedDeltaTime + deltaTime
+
+  local tickTime = 1/60
+
+  while accumulatedDeltaTime > tickTime do
+    fixed_update()
+    accumulatedDeltaTime = accumulatedDeltaTime - tickTime
+  end
+
+end
